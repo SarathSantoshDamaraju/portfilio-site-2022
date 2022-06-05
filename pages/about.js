800874367b092dbedb5f9422689967ef2829835d
sarathdamaraju2007 <sarathdamaraju2007@gmail.com>
2022-06-05 17:10:28 +0530

    fix(page/about): dark mode

--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,19 @@
-import BLOG from '@/blog.config'
-import Container from '@/components/Container'
-import { useLocale } from '@/lib/locale'
+import BLOG from "@/blog.config";
+import Container from "@/components/Container";
+import { useLocale } from "@/lib/locale";
 
 const About = () => {
-  const locale = useLocale()
+  const locale = useLocale();
 
   return (
     <Container>
-      <div className="relative">
+      <div className="relative dark:text-white">
         <p> {locale.ABOUT.INTRO}</p>
         <h2 className="font-ralewayMedium text-author text-primary my-2 hover:underline cursor-text">
           Krishna Damaraju
         </h2>
         <p> {locale.ABOUT.SUMMARY}</p>
-        <div className="my-8 border-4 border-night border-solid	p-4">
+        <div className="my-8 border-4 border-night border-solid	p-4 dark:border-white">
           <span className="text-3xl">🌏</span>
           <ul className="my-2">
             {BLOG.socials.map((each) => (
@@ -21,7 +21,7 @@ const About = () => {
                 <a
                   href={each.link}
                   className="hover:underline hover:text-primary"
-                  target={'_blank'}
+                  target={"_blank"}
                   rel="noopener noreferrer"
                 >
                   {each.name}
@@ -32,7 +32,7 @@ const About = () => {
         </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default About
+export default About;
